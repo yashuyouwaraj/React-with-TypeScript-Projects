4b623fff0ca2eefcd2419a96fcdd49870822a01c
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-16 10:15:57 +0530

    Topic List

--- blogs/src/components/TopicList.tsx
+++ blogs/src/components/TopicList.tsx
@@ -10,8 +10,15 @@ const topics = [
 
 const TopicList = () => {
   return (
-    <div>
-        
+    <div className="bg-white p-4 rounded-lg shadow mt-8">
+        <h3 className="font-semibold text-lg mb-4">Topics for You</h3>
+        <div className="flex flex-wrap gap-2">
+            {topics.map((topic,index)=>(
+                <span className="px-3 py-1 bg-gray-200 text-gray-700 text-sm rounded-full cursor-pointer hover:bg-gray-300">
+                    {topic}
+                </span>
+            ))}
+        </div>
     </div>
   )
 }
