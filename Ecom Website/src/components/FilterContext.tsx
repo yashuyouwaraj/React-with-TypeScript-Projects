4587f3b5110b71416119f63b18a26daf429b768d
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-18 20:08:07 +0530

    Filter Context

---
+++ Ecom Website/src/components/FilterContext.tsx
@@ -0,0 +1,48 @@
+import React, {createContext, useContext, useState, type ReactNode } from "react";
+
+interface FilterContextType{
+    searchQuery:string;
+    setSearchQuery: (query:string)=> void;
+    selectedCategory:string;
+    setSelectedCategory:(category:string)=>void;
+    minPrice:number | undefined
+    setMinPrice:(price:number)=>void;
+    maxPrice:number | undefined
+    setMaxPrice:(price:number)=>void;
+    keyword:string;
+    setKeyword:(keyword:string)=>void;
+}
+
+const FilterContext = createContext<FilterContextType | undefined>(undefined);
+
+export const FilterProvider: React.FC <{children:ReactNode}> =({children})=>{
+    const [searchQuery,setSearchQuery]=useState<string>('')
+    const [selectedCategory,setSelectedCategory]=useState<string>('')
+    const [minPrice,setMinPrice]=useState<number | undefined>(undefined)
+    const [maxPrice,setMaxPrice]=useState<number | undefined>(undefined)
+    const [keyword,setKeyword]=useState<string>('')
+
+    return <FilterContext.Provider value={{
+        searchQuery,
+        setSearchQuery,
+        selectedCategory,
+        setSelectedCategory,
+        minPrice,
+        setMinPrice,
+        maxPrice,
+        setMaxPrice,
+        keyword,
+        setKeyword
+    }}
+    >
+        {children}
+    </FilterContext.Provider>
+}
+
+export const useFilter = ()=>{
+    const context = useContext(FilterContext)
+    if (context===undefined){
+        throw new Error("useFilter must be used within a FilterProvider")
+    }
+    return context;
+}
